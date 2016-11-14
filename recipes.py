c7036c00fc075412b0d33206e427d452d9d44e41
homesmac <jonathan.g.mcwilliams@gmail.com>
2016-11-13 23:57:29 -0800

    Add files via upload

---
+++ recipes.py
@@ -0,0 +1,30 @@
+import random
+
+class ScrappyKNN():
+	def fit(self, X_train, y_train):
+		self.X_train = X_train
+		self.y_train = y_train
+
+	def predict(self, X_test):
+		predictions = []
+		for row in X_test:
+			label = random.choice(self.y_train)
+			predictions.append(label)
+		return predictions
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
+
+my_classifier = ScrappyKNN()
+
+predictions = my_classifier.predict(X_test)
+print predictions
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test, predictions)
